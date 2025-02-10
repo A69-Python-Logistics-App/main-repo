38f470eb77769b12f748f55c88ca05075c3c400f
fcgith <steliyan.slavov31@icloud.com>
2025-02-10 23:02:41 +0200

    Adding CreatePackageCommand

--- core/command_factory.py
+++ core/command_factory.py
@@ -1,3 +1,5 @@
+from commands.base.base_command import BaseCommand
+from commands.create_package_command import CreatePackageCommand
 from core.application_data import ApplicationData
 
 
@@ -5,10 +7,12 @@ class CommandFactory:
     def __init__(self, app_data: ApplicationData):
         self._app_data = app_data
 
-    def create(self, command: str) -> str:
+    def create(self, command: str) -> BaseCommand:
 
         cmd, *params = command.split()
 
-        match cmd:
+        match cmd.lower():
+            case "createpackage":
+                return CreatePackageCommand(params, self._app_data)
             case _:
                 raise ValueError(f"Unknown command: {cmd}")
--- core/engine.py
+++ core/engine.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from core.command_factory import CommandFactory
 
 
@@ -5,8 +7,10 @@ class Engine:
     def __init__(self, cmdf: CommandFactory):
         # read state
         self._command_factory = cmdf
+        self._log = []
 
     def start(self):
+
         print("=" * 10 + " Welcome to Logistics App " + "=" * 10)
         while True:
             cmd = input("> ")
@@ -17,9 +21,14 @@ class Engine:
                 break
 
             try:
-                self._command_factory.create(cmd) # TODO: add execute method
+                command = self._command_factory.create(cmd)
+                log_entry = command.execute()
             except Exception as e:
-                print(e.args[0])
+                log_entry = e.args[0]
+
+            print(log_entry)
+            self._log.append(f"[{datetime.now().strftime("%Y-%m-%d %H-%M-%S")}] " + log_entry)
 
     def stop(self):
+        print("\n".join(self._log))
         print("=" * 10 + " Goodbye " + "=" * 10)
