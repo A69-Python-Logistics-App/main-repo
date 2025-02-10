fa559bd551163f8d03c6e49c4e87f41465a71f2b
fcgith <steliyan.slavov31@icloud.com>
2025-02-10 12:55:53 +0200

    Basic engine implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from core.application_data import ApplicationData
 from core.command_factory import CommandFactory
 from core.engine import Engine
 
+
 app_data = ApplicationData()
 cmdf = CommandFactory(app_data)
 engine = Engine(cmdf)
